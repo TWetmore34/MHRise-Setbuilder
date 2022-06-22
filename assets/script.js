b3877f0bb9ff79f627b96df676db90bda5525962
TWetmore34 <tjwetmore34@gmail.com>
2022-06-21 21:13:41 -0400

    updated functions- first two dropdowns are responsive

--- assets/script.js
+++ assets/script.js
@@ -2,56 +2,62 @@
 
 function Armor(name, skills, defense, resist, slots) {
     this.name = name;
-    this.skills = [skills];
+    this.skills = skills;
     this.defense = defense;
-    this.resist = [resist];
+    this.resist = resist;
     this.slots = slots;
 }
+
+
+console.log(Armor);
+
 // HR Kamura 
 let kamuraHelm = new Armor
 ("Kamura Head Scarf S", 
 ["Divine Blessing x1", "Resuscitate x1"], 
 30,
 [2, 0, 0, 0, 0],
-"single one slot")
+"single one slot");
 
 let kamuraBody = new Armor("Kamura Garb S",
 ["Wirebug Whisperer x2", "Wall Runner x1", "Critical Eye x1"],
 30,
 [2, 0, 0, 0, 0],
-"single one slot")
+"single one slot");
 
 let kamuraBrace = new Armor("Kamura Braces S",
 ["Critical Eye x2", "Heroics x1"],
 30,
 [2, 0, 0, 0, 0],
-"single two slot")
+"single two slot");
 
 let kamuraSkirt = new Armor("Kamura Obi S", 
 ["Critical Eye x1", "Wirebug Whisperer x1"], 
 30,
 [2, 0, 0, 0, 0],
-"double one slot")
+"double one slot");
 
 let kaumraLegs = new Armor("Kamura Leggings S",
 ["Wall Runner x2", "Resuscitate x1"], 
 30,
 [2, 0, 0, 0, 0],
-"single one slot")
+"single one slot");
+
+console.log(kamuraHelm);
 
 // HR Leather
-let leatherHelm = Armor("Leather Headgear S", ["Geologist x1", "Hunger Resistance x1"])
-let leatherBody = Armor("Leather Vest S", ["Botanist x1"])
-let leatherBrace = Armor("Leather Gloves S", ["Geologist x1", "Special Ammo Boost x1"])
-let leatherSkirt = Armor("Leather Belt S", ["Recovery Up x1", "Hunger Resistance x2"])
-let leatherLegs = Armor("Leather Pants S", ["Geologist x1"])
+let leatherHelm = new Armor("Leather Headgear S", ["Geologist x1", "Hunger Resistance x1"])
+let leatherBody = new Armor("Leather Vest S", ["Botanist x1"])
+let leatherBrace = new Armor("Leather Gloves S", ["Geologist x1", "Special Ammo Boost x1"])
+let leatherSkirt = new Armor("Leather Belt S", ["Recovery Up x1", "Hunger Resistance x2"])
+let leatherLegs = new Armor("Leather Pants S", ["Geologist x1"])
 
 // HR Chainmail
-let chainHelm = Armor("Chainmail Headgear S", ["Botanist x1", "Defense Boost x1"])
-let chainBody = Armor("Chainmail Vest S", ["Defense Boost x1", "Flinch Free x2"])
-let chainBrace = Armor("Chainmail Gloves S", ["Botanist x1", "Defense Boost x1"])
-let chainSkirt = Armor("Chainmail Belt S", ["Defense Boost x1", "Guard x1"])
-let chainLegs = Armor("Chainmail Pants S", ["Botanist x1", "Stamina Surge x1"])
+let chainHelm = new Armor("Chainmail Headgear S", ["Botanist x1", "Defense Boost x1"])
+let chainBody = new Armor("Chainmail Vest S", ["Defense Boost x1", "Flinch Free x2"])
+let chainBrace = new Armor("Chainmail Gloves S", ["Botanist x1", "Defense Boost x1"])
+let chainSkirt = new Armor("Chainmail Belt S", ["Defense Boost x1", "Guard x1"])
+let chainLegs = new Armor("Chainmail Pants S", ["Botanist x1", "Stamina Surge x1"])
 // all objects should be sorted by what class of armor they are//
 
 
@@ -60,6 +66,7 @@ let chainLegs = Armor("Chainmail Pants S", ["Botanist x1", "Stamina Surge x1"])
 // updated idea: we do a function for each stat, and set them to run on the button press. Each function displays a different stat so they can each go into the proper text box- the biggest challenge will be sperating the array, but there i can just call it as obj.resists[0] for fire or whahtever
 
 function displayName(obj) {
+    console.log(obj);
     return obj.name;
 }
 
@@ -102,14 +109,86 @@ function display_resist_dragon(obj) {
 function display_slots(obj) {
     return obj.slots;
 }
+// so i think it was these two lines that fixed things, but i dont get how or why yet BUT IT WORKS BABEY lets go
+let kHelm = document.querySelector('[value=kamuraHelm]');
+    kHelm.onclick = function(){document.querySelector('helmName').innerHTML = "waffles"};
 
 function getHelmet() {
+    let kHelm = document.querySelector('[value=kamuraHelm]');
+    kHelm.onclick = function(){document.querySelector('helmName').innerHTML = "waffles"};
     selectElement = document.querySelector('#helmets');
     output = selectElement.value;
-    // this returning as a str is the problem
-    return output;
+    console.log(output);
+
+    // What does the console.log do here? Or was that just checking variables to see how they worked?
+    
+    if (output == 'kamuraHelm') {
+        console.log(kamuraHelm)
+        return kamuraHelm
     }
-    getHelmet()
+
+    if (output == 'leatherHelm') {
+        console.log(leatherHelm)
+        return leatherHelm
+    }
+
+    if (output = 'chainHelm') {
+        console.log(chainHelm)
+        return chainHelm
+    }
+
+    else {
+        console.log("butterfingers!!")
+    }
+}
+
 function helmet(){
     document.getElementById("helmName").innerHTML = displayName(getHelmet());
 }
+
+
+let kBody = document.querySelector('[value=kamuraBody]');
+kHelm.onclick = function(){document.querySelector('bodyName').innerHTML = "waffles"};
+
+function getBody() {
+    let kBody = document.querySelector('[value=kamuraBody]');
+    kBody.onclick = function(){document.querySelector('bodyName').innerHTML = "waffles"};
+    selectElement = document.querySelector('#body');
+    output = selectElement.value;
+    console.log(output);
+
+    if (output == 'kamuraBody') {
+        console.log(kamuraBody)
+        return kamuraBody
+        }
+    if (output == 'leatherBody') {
+        console.log(leatherBody)
+        return leatherBody
+    }
+    if (output == 'chainBody'){
+        console.log(chainBody)
+        return chainBody
+    }
+}
+
+
+function bodyGet(){
+    document.getElementById("bodyName").innerHTML = displayName(getBody());
+}
+
+// Also, what if i combined some global variables + a loop to change them dependant on the inserted obj to make display a little simpler to write
+// let cell1 = "";
+// let cell2 = "";
+
+// function loop(obj) {
+//     for (prop in obj){
+//         cell1 = obj.name;
+//         cell2 = obj.skills;
+// }
+
+// }
+
+// loop()
+
+// console.log(cell1);
+// console.log(cell2);
